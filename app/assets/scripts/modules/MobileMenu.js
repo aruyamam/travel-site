04c85430b2352283c7582008ad4ee7d44163326f
aruyamam <aruyamam@gmail.com>
2018-03-21 10:29:51 +0900

    Completed desktop and mobile header styles.

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -15,8 +15,9 @@ class MobileMenu {
   toggleTheMenu() {
     this.menuContent.toggleClass('site-header__menu-content--is-visible');
     this.siteHeader.toggleClass('site-header--is-expanded');
-    this.menuIcon.toggleClass('site-header__menu-icon--is-close-x');
+    this.menuIcon.toggleClass('site-header__menu-icon--close-x');
   }
+
 }
 
 export default MobileMenu;
